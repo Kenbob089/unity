dd8b53882958f4029fc42feeb38f280861dd2129
Kenbob <129699485+Kenbob089@users.noreply.github.com>
2024-11-18 14:31:43 -0500

    11-18-24

--- Code Practice2/Assets/Arrays.cs
+++ Code Practice2/Assets/Arrays.cs
@@ -25,6 +25,8 @@ public class Arrays : MonoBehaviour
         print(numTwo);
         int[] array1 = { 9, 24, 13, 18, 11 };
         LinearSearch(array1, 13);
+        ConatenationLoop();
+        RandomArray(5);
     }
 
     void LinearSearch(int[] array, int value)
@@ -36,6 +38,32 @@ public class Arrays : MonoBehaviour
             {
                 print(i);
             }
+        }
+    }
+    void ConatenationLoop()
+    {
+        //Interate through the words array, creating a signle long string with spaces between each word.
+        //Hint: In the body of a for loop, add the current value of word at index to a string
+        //then a space using  += " ";
+
+        string[] words = { "The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+
+        string finalString = "";
+        for (int i = 0;i < words.Length;i++)
+        {
+            finalString = finalString + words[i] + " ";
+        }
+        print(finalString);
+    }
+    void RandomArray(int size)
+    {
+        //Declare and intinalize a new array of intergers with length [size]
+        //interate through the array setting the value at each index to a random number between one and ten
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Random.Range(1, 11);
         }
     }
     // Update is called once per frame
